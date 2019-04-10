5ddde41dfc1714291d895bc2972cb7917f2e4c9d
serii <seriiburduja@gmail.com>
2019-04-10 15:12:52 +0300

    + search

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,52 +1,70 @@
+'use strict';
+
 $(function () {
   /*
       https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
       ===========================*/
-  $("html").easeScroll({
-    frameRate: 60,
-    animationTime: 1000,
-    stepSize: 90,
-    pulseAlgorithm: 1,
-    pulseScale: 8,
-    pulseNormalize: 1,
-    accelerationDelta: 20,
-    accelerationMax: 1,
-    keyboardSupport: true,
-    arrowScroll: 50,
-    touchpadSupport: true,
-    fixedBackground: true
-  });
-
-	//show menu
-	let closeMenu = $('#js-close-menu');
-	let showMenu = $('#js-show-menu');
-	let navMenu = $('#js-nav-menu');
-
-
-	closeMenu.on('click', function(){
-		closeMenu.hide();
-		showMenu.show();
-		navMenu.slideUp();
+	$("html").easeScroll({
+		frameRate: 60,
+		animationTime: 1000,
+		stepSize: 90,
+		pulseAlgorithm: 1,
+		pulseScale: 8,
+		pulseNormalize: 1,
+		accelerationDelta: 20,
+		accelerationMax: 1,
+		keyboardSupport: true,
+		arrowScroll: 50,
+		touchpadSupport: true,
+		fixedBackground: true
 	});
 
-	showMenu.on('click', function(){
-		closeMenu.show();
-		showMenu.hide();
-		navMenu.slideDown();
-	});
+	//drop down menu
 
-	//reviews-slider
-	$('#js-reviews-slider').slick({
+	let mainNavDropdowItemHover = () => {
+		$('.main-nav-dropdown__item').hover(function (e) {
+			// Получаем родителя
+			let parentList = $(this).closest('.main-nav-dropdown__list');
+			let childrenList = $(this).children('.main-nav-dropdown__list-sub');
 
-	});
+			if (childrenList.length) {
+				parentList.css('width', '720px');
 
-	let videoImage = $('#js-video__img');
-	let videoPlay = $('#js-video__play');
+				if (parentList.outerHeight() < childrenList.outerHeight()) {
+					parentList.css('height', childrenList.outerHeight());
+				}
+			}
+		}, function () {
+			let parentList = $(this).closest('.main-nav-dropdown__list');
+			parentList.css('width', 'auto');
+			parentList.css('height', 'auto');
+		});
+	}
 
-	videoPlay.on('click', function(){
-		videoPlay.fadeOut();
-		videoImage.fadeOut();
-	});
+	//show search form
+	let searchForm = () => {
+		let headerSearchIcon = $('.header-search-icon');
+
+		headerSearchIcon.on('click', function () {
+			$(this).parent().addClass('search--open');
+		});
+	}
+
+	let clearSearchForm = () => {
+		$('#js-search__clear .search__icon').on('click', function () {
+			$('#js-search__input').val('');
+		});
+	}
+
+	let closeSearchForm = () => {
+		$('.search__form').on('submit', function () {
+			$('.main-nav__search').removeClass('search--open');
+		});
+	}
+
+	mainNavDropdowItemHover();
+	searchForm();
+	clearSearchForm();
 
 });
 
